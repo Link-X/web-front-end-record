223b2bec58dd7547b332762bc720780820693da9
xudaobin <1555537368@163.com>
2020-10-21 11:56:28 +0800

    init: rollup 配置

---
+++ dist/web-front-end-record.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var index = 42;
+
+function main () {
+  var a = 1;
+  console.log("asdf".concat(a).concat(index));
+}
+
+module.exports = main;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import foo from './foo.js'
+
+export default function() {
+  const a = 1
+  console.log(`asdf${a}`)
+}
