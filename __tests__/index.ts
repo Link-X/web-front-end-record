0192cd3f0a936ad37feabd521838db66b50f14cf
xudaobin <1555537368@163.com>
2020-11-03 10:16:19 +0800

    =

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,2 +1,11 @@
 import record from '../src/index'
 
+
+test('null', () => {
+  const n: any = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
